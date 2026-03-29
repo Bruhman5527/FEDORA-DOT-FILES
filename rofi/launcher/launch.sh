c72d0a39449ed482f26e9834b235630544a39bf1
Tobias Molzer <molzertobias@gmail.com>
2026-03-29 13:08:34 +0200

    Worked on Rofi, launcher almost done except a few silly bugs.

---
+++ rofi/launcher/launch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+CONFIG="/home/Bruhman5527/Documents/DOT-FILES/rofi/launcher/config.rasi"
+THEME="/home/Bruhman5527/Documents/DOT-FILES/rofi/launcher/style.rasi"
+
+rofi -show drun -config $CONFIG
